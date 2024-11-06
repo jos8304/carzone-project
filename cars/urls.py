a29aedb13fdca6e286eb5942e0f17abb5c0afc1a
jos8304 <jyou8304@gmail.com>
2024-11-05 21:55:53 -0500

    added search function

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from . import views # Import your views module
+from .views import car_detail
 
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:id>',view=car_detail,name='car_detail'),
+    path('search', views.search, name='search'),
 ]
